b0d8442ba52b9b09f6b4e07b88bbc443e23e2c0a
Abby Nyakara <abbynyakara@gmail.com>
2022-06-01 11:22:22 +0300

    correct linter errors

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ class Book {
   }
 }
 
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+
 // Handle UI task
 
 class UI {
@@ -18,8 +21,6 @@ class UI {
   static addBookToList(book) {
     const list = document.querySelector('.table');
     const row = document.createElement('tr');
-    const title = document.querySelector('#title');
-    const author = document.querySelector('#author');
     const tableData = document.createElement('td');
     tableData.classList.add('titleList');
     row.appendChild(tableData);
@@ -39,7 +40,7 @@ class UI {
 
   static deleteBook(el) {
     if (el.classList.contains('delete')) {
-       el.parentElement.parentElement.remove();
+      el.parentElement.parentElement.remove();
     }
   }
   static clearFields() {
@@ -52,21 +53,21 @@ class UI {
 // Store class: Handle storage
 class Store {
   static getBooks () {
-      let books;
-      if (localStorage.getItem('books') === null) {
+    let books;
+    if (localStorage.getItem('books') === null) {
       books = [];
     }
     else {
       books = JSON.parse(localStorage.getItem('books'));
     }
-  return books;
+    return books;
   }
   static addBook(book) {
-const books = Store.getBooks();
-// books.push(book);
+    const books = Store.getBooks();
+    // books.push(book);
 
-localStorage.setItem('books', JSON.stringify(books));
-}
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 
 // event display book
@@ -80,12 +81,12 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   // Add book to UI
   UI.addBookToList(book);
   // Add book to store
-  Store.addBook(book)
+  Store.addBook(book);
   // clear fields
   UI.clearFields();
 });
 
-// Event: remove a book 
+// Event: remove a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
   UI.deleteBook(e.target);
 });
