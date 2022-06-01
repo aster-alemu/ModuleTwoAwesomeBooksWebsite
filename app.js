387f368a43e0df4c265506d763f9b3027e4c5d35
Abby Nyakara <abbynyakara@gmail.com>
2022-06-01 11:30:55 +0300

    correct linter errors

--- app.js
+++ app.js
@@ -56,10 +56,8 @@ class Store {
   static getBooks() {
     let books;
     if (localStorage.getItem('books') === null) {
-      books = [];
-    }
-    
-    else {
+      books = []; 
+    } else {
       books = JSON.parse(localStorage.getItem('books'));
     }
     return books;
